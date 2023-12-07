7878d9292c5e72756821b2d787b7189184f61db0
kshitij_saini <kshitizsaini1941996@gmail.com>
2023-12-08 03:16:27 +0530

    lecture 2 done

--- Assignment_2/App.jsx
+++ Assignment_2/App.jsx
@@ -1,10 +1,11 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const App = () => {
   return (
     <div>
       <div>Hello</div>
     </div>
-  )
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
